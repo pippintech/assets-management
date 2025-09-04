28e10b2cc30e0c119a68ab3e2c1421e0cf57ae8e
Karthikeyan <karthikeyan@pippintitle.com>
2025-09-04 13:23:45 +0530

    new35

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://34.45.206.133:${3000}`);
 });
